6cc3c81dbe880fb12985ee6516ff6c5c0a97a083
Ariikii30 <maria.cont64@gmail.com>
2024-12-17 13:30:19 +0100

    Books

---
+++ EscapeRoom/Assets/Art/3D Models/bookColor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookColor : MonoBehaviour
+{
+    [SerializeField]
+    private Color m_Color = new Color();
+
+    private Renderer objectRenderer;
+    private Material objectMaterial;
+
+    private void OnValidate()
+    {
+       objectRenderer = GetComponent<Renderer>();
+
+        if (objectRenderer != null)
+        {
+            if (objectMaterial == null) 
+            {
+                objectMaterial = new Material(objectRenderer.sharedMaterial);
+                objectRenderer.material = objectMaterial;   
+            }
+            objectMaterial.color = m_Color;
+        }
+    }
+
+}
