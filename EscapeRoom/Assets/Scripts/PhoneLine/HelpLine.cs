dcda2ef4d4cb989f184dd753ba3bd932209b5a5b
Leah <45321184+ChaoticLeah@users.noreply.github.com>
2025-01-13 16:36:15 +0100

    start of the phone

---
+++ EscapeRoom/Assets/Scripts/PhoneLine/HelpLine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpLine : MonoBehaviour
+{
+    public enum STATES {
+        INTRO,
+        MAIN_PUZZLE_AREA, // The area that goes into find laptop password, solve binary book puzzles, and find QR code
+        DECRYPT_PASSWORD // At the end when 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
